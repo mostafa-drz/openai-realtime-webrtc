b72ae0ca96d311476884098b53eab7e02796dada
Mostafa Darehzereshki <mostafa69d@gmail.com>
2025-02-13 20:03:14 -0500

    chore: fixes the wrong stage reference

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,4 +1,10 @@
-export default {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  '*.{json,md,yml,yaml}': ['prettier --write'],
+const config = {
+  'src/**/*.{js,jsx,ts,tsx}': [
+    'npm run lint:fix',
+    'npm run format',
+    () => 'npm run ts:check',
+  ],
+  'src/**/*.{json,md,yml,yaml}': ['npm run format'],
 };
+
+export default config;
