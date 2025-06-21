5d5e3df339fa7d80700b436f9bbcdadd121e906d
Mostafa Darehzereshki <mostafa69d@gmail.com>
2025-06-21 13:09:39 -0400

    removes old code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,7 @@
 'use client';
 
 import './globals.css';
-import { OpenAIRealtimeWebRTCProvider } from './context/OpenAIRealtimeWebRTC';
-import {
-  Modality,
-  OpenAIRealtimeContextConfig,
-  TurnDetectionType,
-  Voice,
-  Logger,
-  LogLevel,
-} from './types';
+import { Logger, LogLevel } from './types';
 
 // Move the logger creation to a client component
 const ClientProvider = ({ children }: { children: React.ReactNode }) => {
@@ -59,43 +51,7 @@ const ClientProvider = ({ children }: { children: React.ReactNode }) => {
     };
   };
 
-  const defaultConfig: OpenAIRealtimeContextConfig = {
-    realtimeApiUrl: 'https://api.openai.com/v1/realtime',
-    modelId: 'gpt-4o-realtime-preview-2024-12-17',
-    // logger: createFancyLogger(), // Create logger on the client side
-    // logger: console,
-    logger: {
-      debug: () => {},
-      info: () => {},
-      warn: () => {},
-      error: (...all) => {
-        console.error(...all);
-      },
-    },
-    defaultSessionConfig: {
-      modalities: [Modality.TEXT, Modality.AUDIO],
-      input_audio_transcription: {
-        model: 'whisper-1',
-      },
-      voice: Voice.ALLOY,
-      instructions: `
-        You are a fortune teller. You can see the future.
-      `,
-      turn_detection: {
-        type: TurnDetectionType.SERVER_VAD,
-        threshold: 0.5,
-        prefix_padding_ms: 300,
-        silence_duration_ms: 500,
-      },
-      connection_timeout: 10000,
-    },
-  };
-
-  return (
-    <OpenAIRealtimeWebRTCProvider config={defaultConfig}>
-      {children}
-    </OpenAIRealtimeWebRTCProvider>
-  );
+  return <>{children}</>;
 };
 
 // Keep the RootLayout as a server component
